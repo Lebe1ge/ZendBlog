bd9415fc0ed085f80eff58f564422052192bf55a
Juicy <julien.decoen@gmail.com>
2016-02-07 16:35:42 +0100

    Start admin with post & cat

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -1,3 +1,171 @@
 <?php
+namespace Admin;
+
 return array(
+    'router' => array(
+        'routes' => array(
+            'zfcadmin' => array(
+                'child_routes' => array(
+                    'category' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route' => '/category[/:action][/:id]',
+                            'constraints' => array(
+                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id' => '[0-9]+',
+                            ),
+                            'defaults' => array(
+                                'controller' => 'Admin\Controller\Category',
+                                'action' => 'index'
+                            ),
+                        ),
+                        'may_terminate' => true,
+                        'child_routes' => array(
+                            'action' => array(
+                                'type' => 'segment',
+                                'options' => array(
+                                    'route' => '/category/:action[/:id]',
+                                    'constraints' => array(
+                                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                        'id' => '[0-9]+',
+                                    ),
+                                    'defaults' => array(
+                                        'controller' => 'Admin\Controller\Category',
+                                        'action' => 'index',
+                                    )
+                                ),
+                                'may_terminate' => true
+                            )
+                        )
+                    ),
+                    'post' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route' => '/post[/:action][/:id]',
+                            'constraints' => array(
+                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id' => '[0-9]+',
+                            ),
+                            'defaults' => array(
+                                'controller' => 'Admin\Controller\Post',
+                                'action' => 'index'
+                            ),
+                        ),
+                        'may_terminate' => true,
+                        'child_routes' => array(
+                            'action' => array(
+                                'type' => 'segment',
+                                'options' => array(
+                                    'route' => '/post/:action[/:id]',
+                                    'constraints' => array(
+                                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                        'id' => '[0-9]+',
+                                    ),
+                                    'defaults' => array(
+                                        'controller' => 'Admin\Controller\Post',
+                                        'action' => 'index',
+                                    )
+                                ),
+                                'may_terminate' => true
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    ),
+    'navigation' => array(
+        'admin' => array(
+            // Profondeur 0
+            'index' => array(
+                'label' => 'Admin',
+                'route' => 'zfcadmin',
+                'pages' => array(
+                    // Profondeur 1
+                    'category' => array(
+                        'label' => 'Category',
+                        'route' => 'zfcadmin/category',
+                        'pages' => array(
+                            'new_category' => array(
+                                'label' => 'New category',
+                                'route' => 'zfcadmin/category/action',
+                                'params' => array('action' => 'add')
+                            ),
+                            'edit_category' => array(
+                                'label' => 'Edit category',
+                                'route' => 'zfcadmin/category/action',
+                                'params' => array('action' => 'edit')
+                            )
+                        )
+                    ),
+                    // Profondeur 1
+                    'post' => array(
+                        'label' => 'Job',
+                        'route' => 'zfcadmin/post',
+                        'pages' => array(
+                            'new_post' => array(
+                                'label' => 'New post',
+                                'route' => 'zfcadmin/post/action',
+                                'params' => array('action' => 'add')
+                            ),
+                            'edit_post' => array(
+                                'label' => 'Edit post',
+                                'route' => 'zfcadmin/post/action',
+                                'params' => array('action' => 'edit')
+                            )
+                        )
+                    ),
+                )
+            )
+        )
+    ),
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ),
+        'factories' => array(
+            'Application\Service\PostService' => 'Application\Service\Factory\PostServiceFactory',
+            'Application\Service\CategoryService' => 'Application\Service\Factory\CategoryServiceFactory'
+        ),
+        'aliases' => array(
+            'translator' => 'MvcTranslator',
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Admin\Controller\Category' => 'Admin\Controller\CategoryController',
+            'Admin\Controller\Post' => 'Admin\Controller\PostController'
+        ),
+    ),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/admin.phtml',
+            'admin/index/index' => __DIR__ . '/../view/admin/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            'Application_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../../Application/src/Application/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'Application\Entity' =>'Application_driver'
+                )
+            )
+        )
+    ),
 );
---
+++ module/Admin/src/Admin/Controller/PostController.php
@@ -0,0 +1,57 @@
+<?php
+namespace Admin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query;
+use Zend\View\Model\ViewModel;
+use Application\Form\CategoryForm;
+use Application\Entity\Category;
+
+class PostController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        $posts = $this->getServiceLocator()->get('Application\Service\PostService')->getAll();
+        return new ViewModel(array(
+            'posts' => $posts,
+        ));
+    }
+
+    public function addAction()
+    {
+        $formPost = new PostForm();
+
+        // On récupère l'objet Request
+        $request = $this->getRequest();
+
+        // On vérifie si le formulaire a été posté
+        if ($request->isPost()) {
+            // On instancie notre modèle Post
+            $post= new Post();
+
+            // Et on passe l'InputFilter de Post au formulaire
+            $formPost->setInputFilter($post->getInputFilter());
+            $formPost->setData($request->getPost());
+
+            // Si le formulaire est valide
+            if ($formPost->isValid()) {
+                // On prend les données du formulaire qui sont converti pour correspondre à notre modèle Post
+                $post->exchangeArray($formPost->getData());
+
+                // On enregistre ces données dans la table Post
+                $this->getServiceLocator()->get('Application\Service\PostService')->savePost($post);
+
+                // Puis on redirige sur la page d'accueil.
+                return $this->redirect()->toUrl('/');
+            }
+            // Si le formulaire n'est pas valide, on reste sur la page et les erreurs apparaissent
+        }
+
+        return new ViewModel(
+            array(
+                'form' => $formPost
+            )
+        );
+    }
+}
