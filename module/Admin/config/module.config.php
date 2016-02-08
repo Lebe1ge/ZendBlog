7aa3e638e7c37f462c8b419949dac6c1a4b01e98
Juicy <julien.decoen@gmail.com>
2016-02-08 19:47:59 +0100

    Erreur layout

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -145,7 +145,7 @@ return array(
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/admin.phtml',
+            'layout/admin'           => __DIR__ . '/../view/layout/admin.phtml',
             'admin/index/index' => __DIR__ . '/../view/admin/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
