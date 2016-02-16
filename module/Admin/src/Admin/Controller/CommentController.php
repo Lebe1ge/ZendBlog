f37524788afaadf70e01a27748ee74c8e0eff13f
Lebe1ge <julien.decoen@gmail.com>
2016-02-16 16:05:00 +0100

    Commentaire Admin done

--- module/Admin/src/Admin/Controller/CommentController.php
+++ module/Admin/src/Admin/Controller/CommentController.php
@@ -11,122 +11,29 @@ class CommentController extends AbstractActionController
 {
     public function indexAction()
     {
-        $posts = $this->getServiceLocator()->get('Application\Service\PostService')->getAll();
-        return new ViewModel(array(
-            'posts' => $posts,
-        ));
-    }
-
-    public function addAction()
-    {
-        $formPost = new PostForm();
-
-        // On récupère l'objet Request
-        $request = $this->getRequest();
-
-        // On vérifie si le formulaire a été posté
-        if ($request->isPost()) {
-
-            // On instancie notre modèle Post
-            $post= new Comment();
-
-            // Et on passe l'InputFilter de Post au formulaire
-            $postInput = $post->getInputFilter();
-            $formPost->setInputFilter($postInput);
-            $formPost->setData($request->getPost());
-            // Si le formulaire est valide
-            if ($formPost->isValid()) {
-
-                try{
-                    // On prend les données du formulaire qui sont converti pour correspondre à notre modèle Post
-                    $post->exchangeArray($formPost->getData());
-
-                    // On enregistre ces données dans la table Post
-                    $this->getServiceLocator()->get('Application\Service\PostService')->savePost($post);
-                    $this->flashMessenger()->addMessage(array('success' => "Article '{$post->post_id}' posté avec succès"));
-                    // Puis on redirige sur la page d'accueil.
-                    return $this->redirect()->toRoute('zfcadmin/post');
-                }catch(\Exception $e){
-
-                    die($e->getMessage());
-                }
-            }
-            else{
-
-                $formPost->getMessages();
-            }
-            // Si le formulaire n'est pas valide, on reste sur la page et les erreurs apparaissent
+        $comments = $this->getServiceLocator()->get('Application\Service\CommentService')->getAll();
+        foreach($comments as $comment){
+            $comment->article = $this->getServiceLocator()->get('Application\Service\PostService')->getById($comment->post_id);
         }
-
-        return new ViewModel(
-            array(
-                'form' => $formPost
-            )
-        );
+        return new ViewModel(array(
+            'comments' => $comments,
+            'flashMessages' => $this->flashMessenger()->getMessages()
+        ));
     }
 
     public function deleteAction()
     {
         try{
-            $idPost = $this->params('id');
+            $comment = $this->getServiceLocator()->get('Application\Service\CommentService')->getById($this->params('id'));
+            $comment_author = $comment->author;
+            $this->getServiceLocator()->get('Application\Service\CommentService')->deleteComment($comment->comment_id);
+
+            $this->flashMessenger()->addMessage(array('success' => "Le commentaire de '{$comment_author}' a été supprimée"));
+            // Puis on redirige sur la page d'accueil.
+            return $this->redirect()->toRoute('zfcadmin/comment');
+
         } catch( \InvalidArgumentException $e ){
-
-            return $this->redirect()->toRoute('zfcadmin/post');
+            return $this->redirect()->toRoute('zfcadmin/comment');
         }
-
-        $this->getServiceLocator()->get('Application\Service\PostService')->deletePost($idPost);
-        return $this->redirect()->toRoute('zfcadmin/post');
-
-    }
-
-    public function editAction()
-    {
-        $idPost = $this->params('id');
-        $formPost = new PostForm();
-        $postData = $this->getServiceLocator()->get('Application\Service\PostService')->getById($idPost);
-        $formPost->bind($postData);
-        // On récupère l'objet Request
-        $request = $this->getRequest();
-
-        // On vérifie si le formulaire a été posté
-        if ($request->isPost()) {
-            // On instancie notre modèle Post
-            $post= new Comment();
-
-            // Et on passe l'InputFilter de Post au formulaire
-            $formPost->setInputFilter($post->getInputFilter());
-            $formPost->setData($request->getPost());
-
-            // Si le formulaire est valide
-            if ($formPost->isValid()) {
-
-                try{
-                    // On prend les données du formulaire qui sont converti pour correspondre à notre modèle Post
-                    $post->exchangeArray($formPost->getData());
-
-                    // On enregistre ces données dans la table Post
-                    $this->getServiceLocator()->get('Application\Service\PostService')->savePost($post);
-                    //$this->flashMessenger()->addMessage(array('success' => "Category '{$post->name}' was added successfully"));
-                    // Puis on redirige sur la page d'accueil.
-                    return $this->redirect()->toRoute('zfcadmin/post');
-
-                } catch(\Exception $e){
-                    die($e->getMessage());
-                }
-
-
-            }
-            // Si le formulaire n'est pas valide, on reste sur la page et les erreurs apparaissent
-        }else{
-
-            $formPost->getMessages();
-        }
-
-        return new ViewModel(
-            array(
-                'form' => $formPost,
-                'id'   => $idPost
-            )
-        );
     }
 }
